f41402ae0e8fe8f4743b724d58c621cf2d925fcd
Ashutosh Rathi <ashutosh@komprise.com>
2022-04-03 23:12:53 +0530

    added try catch

--- pages/api/addOrder.js
+++ pages/api/addOrder.js
@@ -11,19 +11,34 @@ export default async function handler(req, res) {
   }
   const { quality, rate, weaver, payment, quantity, partyName, weaverPrice } = req.body;
 
-  const result =  await prisma.order.create({
-    data: {
-      'quality': quality,
-      'quantity': quantity,
-      'rate': rate,
-      'weaverName': weaver,
-      'weaverPrice': weaverPrice,
-      'partyName': partyName,
-      'payment': payment,
-    },
-  });
+
+  try {
+    const result =  await prisma.order.create({
+      data: {
+        'quality': quality,
+        'quantity': quantity,
+        'rate': rate,
+        'weaverName': weaver,
+        'weaverPrice': weaverPrice,
+        'partyName': partyName,
+        'payment': payment,
+      },
+    });
+    res.status(200).send(result);
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (e.code === 'P2002') {
+        console.log(
+          'There is a unique constraint violation, a new user cannot be created with this email'
+        )
+      }
+    }
+    throw e;
+    }
+
+  
   // res.json(result);
-  res.status(200).send(result);
 
 
   // the rest of your code
